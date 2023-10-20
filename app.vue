198157600f673bab528062c81beac7b6fbd1f190
OvalniyKeks <keksovalniy@gmail.com>
2023-10-20 06:16:53 +0300

    fix layout

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
-<script setup lang="ts">
-const { data } = await useFetch('/api/hello')
-</script>
-
 <template>
-  <div>
-    Hello World
-    {{ data }}
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
--- pages/README.md
+++
@@ -1,6 +0,0 @@
-# Pages
-
-<h3><a href="https://nuxt.com/docs/guide/directory-structure/pages">Документация</a></h3>
-
-##
-
